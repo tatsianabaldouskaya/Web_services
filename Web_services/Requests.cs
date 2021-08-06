cfcb9d79a2ac7f61520ce5eca0d6101e8df73b69
Tatsiana Baldouskaya <tatsiana_baldouskaya@epam.com>
2021-08-06 21:36:11 +0300

    Add project files.

---
+++ Web_services/Requests.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_services
+{
+    public static class Requests
+    {
+
+        public static HttpWebResponse MakeGetRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("https://jsonplaceholder.typicode.com/users");
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            return response;
+        }
+        public static string GetResponseBody()
+        {
+            string responseBody = String.Empty;
+            
+            using (Stream s = MakeGetRequest().GetResponseStream())
+            {
+                using (StreamReader r = new StreamReader(s))
+                {
+                    responseBody = r.ReadToEnd();
+                }
+            }
+            return responseBody;
+        }
+
+        public static HttpStatusCode GetStatusCode()
+        {
+            HttpStatusCode statusCode = MakeGetRequest().StatusCode;
+            return statusCode;
+        }
+
+        public static string GetResponseHeader()
+        {
+            string responceHeader = MakeGetRequest().GetResponseHeader("Content-Type");
+            return responceHeader;
+        }
+
+        public static HttpWebResponse MakePostRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("https://jsonplaceholder.typicode.com/users");
+            request.Method = "POST";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (Stream stream = request.GetRequestStream())
+            {
+                using (StreamWriter streamWriter = new StreamWriter(stream))
+                {
+                    streamWriter.Write("id: 11");
+                }
+            }
+            return response;
+        }
+
+
+    }
+}
---
+++ Web_services/Tests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Net.Http;
+using System;
+using Newtonsoft.Json;
+
+namespace Web_services
+{
+    [TestFixture]
+    [Parallelizable]
+    public class Tests
+    {
+        [Test]        
+        public void HttpStatusCodeTest()
+        {
+           Assert.AreEqual("OK", Requests.GetStatusCode().ToString(), "Status code is NOT 200");
+        }
+
+        [Test]
+        public void HttpResponseHeaderTest()
+        {
+            Assert.AreEqual("application/json; charset=utf-8", Requests.GetResponseHeader(), "Response header doesn't orrespond to expected");
+        }
+
+        [Test]
+        public void HttpResponseBodyTest()
+        {
+            Root[] users = JsonConvert.DeserializeObject<Root[]>(Requests.GetResponseBody());
+            Assert.AreEqual(10, users.Length, "Users quantity is not 10");
+        }
+    }
+}
